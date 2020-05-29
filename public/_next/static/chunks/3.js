af8d958110fc840af72ba5cb57c63cb7cb702180
chenzhao <chenzhao@ccbscf.com>
2020-05-29 23:02:54 +0800

    next blog

---
+++ public/_next/static/chunks/3.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ "./content/posts/2017/post-two.json":
+/*!******************************************!*\
+  !*** ./content/posts/2017/post-two.json ***!
+  \******************************************/
+/*! exports provided: title, date, bodyContent, bodyHtml, preview, dir, base, ext, sourceBase, sourceExt, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"title\\\":\\\"Post two\\\",\\\"date\\\":\\\"2017-06-12T00:00:00.000Z\\\",\\\"bodyContent\\\":\\\"Second post!\\\\n\\\\nCheck out [GitHub to see my other projects](https://github.com/tscanlin)\\\",\\\"bodyHtml\\\":\\\"<p>Second post!</p>\\\\n<p>Check out <a href=\\\\\\\"https://github.com/tscanlin\\\\\\\">GitHub to see my other projects</a></p>\\\\n\\\",\\\"preview\\\":\\\"Second post!\\\\n\\\\nCheck out GitHub to see my other projects\\\",\\\"dir\\\":\\\"content/posts/2017\\\",\\\"base\\\":\\\"post-two.json\\\",\\\"ext\\\":\\\".json\\\",\\\"sourceBase\\\":\\\"post-two.md\\\",\\\"sourceExt\\\":\\\".md\\\"}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2NvbnRlbnQvcG9zdHMvMjAxNy9wb3N0LXR3by5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./content/posts/2017/post-two.json\n");
+
+/***/ })
+
+}]);
